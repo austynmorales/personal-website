2cf296e398d314d2812e08dfc5bbee66aec0bd90
Austyn Morales <amorales14@buffs.wtamu.edu>
2026-09-07 01:09:33 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    items = [
+        {'name': 'Learn Django', 'completed': True},
+        {'name': 'Build a personal website', 'completed': True},
+        {'name': 'Learn Python', 'completed': False},
+        {'name': 'Create Portfolio', 'completed': True},
+        {'name': 'Finish Web Development Course', 'completed': False},
+    ]
+
+    return render(request, 'home.html', {'items': items})
 
 def about(request):
     return render(request, 'about.html')
